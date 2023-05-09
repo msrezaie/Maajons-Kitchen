6bee707c44170d014db11f67e29afbd0a92ca9b7
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-05-09 13:00:03 -0400

    'node-fied' the static app

---
+++ app.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+
+const mainRoutes = require('./routes/main');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(mainRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(5001);
